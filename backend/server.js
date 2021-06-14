fe5a6e797bcda443dfc03886184cf1a4ff1a4b32
Robin Biju <robinbiju8111@gmaili.com>
2021-06-14 23:08:13 +0530

    local server created and load products from that server, maintained seperate folder for fontend and backend

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import data from './Productdata.js';
+
+const app = express();
+app.get('/api/products', (req, res) => {
+    res.send(data.products);
+});
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`serve at http://localhost:${port}`);
+});
